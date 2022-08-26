4483fdc593dd09748314710e2ef62bf1dddf1ac1
FireNoobsta <FireNooobsta@gmail.com>
2022-08-25 19:20:53 -0700

    Add experimental map and obstacle course map

---
+++ LightBearersMOD/InstallMOD.bat
@@ -0,0 +1,73 @@
+@ECHO OFF
+ECHO Welcome to the LightBearers mod installer!
+ECHO Make sure you have read the README file first.
+ECHO First type the filepath for the pak file of LightBearers, it should be called "LightBearers-WindowsNoEditor.pak"
+ECHO (WARNING: This file will be deleted so make sure it is the right file!)
+set /p PAKFILEPATH="LightBearers Pakfile path: "
+
+ECHO Now type the filepath for the unrealpak.exe file in your installation of unreal engine.
+set /p UNREALPAKPATH="Unrealpak.exe path: "
+
+::Setting up the dataToPack file
+chdir> temp.txt
+set /p CurrDir=<temp.txt
+echo ^"%CurrDir%\ModFiles\Extract\Engine\*^" ^"../../../Engine/^"> .\ModFiles\dataToPack.txt
+echo ^"%CurrDir%\ModFiles\Extract\LightBearers\*^" ^"../../../LightBearers/^">> .\ModFiles\dataToPack.txt
+
+
+::Extracting the pak file
+%UNREALPAKPATH% %PAKFILEPATH% -Extract %CurrDir%\ModFiles\Extract
+
+ECHO You should have seen a lot of lines saying extractions were made.
+ECHO If not then an error occurred and you should close the program now. Otherwise continue
+PAUSE
+
+::Replacing files in Extract
+del %CurrDir%\ModFiles\Extract\LightBearers\Config\DefaultInput.ini
+copy %CurrDir%\ModFiles\DefaultInput.ini %CurrDir%\ModFiles\Extract\LightBearers\Config
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting.uexp
+copy %CurrDir%\ModFiles\Advanced_Lighting.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting.umap
+copy %CurrDir%\ModFiles\Advanced_Lighting.umap %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.uasset
+copy %CurrDir%\ModFiles\Advanced_Lighting_BuiltData.uasset %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.ubulk
+copy %CurrDir%\ModFiles\Advanced_Lighting_BuiltData.ubulk %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.uexp
+copy %CurrDir%\ModFiles\Advanced_Lighting_BuiltData.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap.uexp
+copy %CurrDir%\ModFiles\StarterMap.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap.umap
+copy %CurrDir%\ModFiles\StarterMap.umap %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.uasset
+copy %CurrDir%\ModFiles\StarterMap_BuiltData.uasset %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.ubulk
+copy %CurrDir%\ModFiles\StarterMap_BuiltData.ubulk %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.uexp
+copy %CurrDir%\ModFiles\StarterMap_BuiltData.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+
+
+::Deleting pakfile and repaking
+ECHO This next part may take a while, please be patient
+del %PAKFILEPATH%
+%UNREALPAKPATH% %PAKFILEPATH% -Create=%CurrDir%\ModFiles\dataToPack.txt
+
+::Deleting Extract, we don't need it anymore
+::del %CurrDir%\ModFiles\Extract\Engine
+::del %CurrDir%\ModFiles\Extract\LightBearers
+
+ECHO The mod should be installed now!
+ECHO Refer to the README to find the relevant console commands.
+PAUSE
---
+++ LightBearersMOD/README.txt
@@ -0,0 +1,47 @@
+#-----------------------------------
+# Notes
+#-----------------------------------
+This is a mod for the current version of LightBearers as of 8/2/2022. NOT for the public beta. It also uses a windows batch script to install.
+I recommend creating a copy of your entire game directory, once installed you can launch the mod from the executable in your modded game directory,
+and still play vanilla by launching through steam.
+To install this mod you'll need two things:
+1. An installation of LightBearers (Obviously)
+2. An installation of Unreal Engine version 4.22.3
+It's important that you use this exact version since it is what LightBearers uses (for now).
+I can't provide a direct link for unreal engine since you have to use the Epic games laucher, but heres 
+a link to download that https://www.unrealengine.com/en-US/download.
+Once you have those you should be good to go. Also make sure you have enough room in your drive because this script will extract all of the game files
+to this directory which is around 8-9 GB.
+This mod is pretty bare bones though. Right now it will only unlock the dev console and add custom maps I made.
+
+#----------------------------------
+# Installation
+#----------------------------------
+Make sure you extracted the .zip file to a drive with 8-9 GB of available space.
+
+Locate your LightBearers pakfile. It should be called LightBearers-WindowsNoEditor.pak and it is somewhere in \LightBearers\Content\Paks.
+Locate your Unreal Engine Pak executable. It is called UnrealPak.exe and it is in your unreal engine installation, somewhere in Epic Games\UE_4.22\Engine\Binaries\Win64.
+Have both of those  files ready in windows explorer.
+
+Now run the batch script called InstallMOD. This will run a number of commands in cmd prompt and will ask for input.
+When it asks for file paths, you can type the full file path for the file, or you can drag the file from the windows explorer to the command prompt
+and it will type the full path for you. Make sure you drag the correct files though.
+
+If you have the correct version of unreal engine then this should install the mod after a few minutes.
+The extracted game files are left in the Extract folder and you can delete them to make space if you want, they are no longer needed.
+
+#-----------------------------------
+# Accessing the mod 
+#-----------------------------------
+This "mod" adds very little. As said before, now you will have access to the dev console with "4" in game.
+Here are some useful commands:
+Open <mapname>                           This will load a specified map. Look in Extract\LightBearers\Content\ThirdPersonBP\Maps for the names of maps in files.
+ServerTravel <mapname>                   If you are hosting a lobby, this will load a specified map and take everyone in the server with you.
+
+To access the custom maps type this into the console.
+Open Advanced_Lighting
+Open StarterMap
+
+
+
+
