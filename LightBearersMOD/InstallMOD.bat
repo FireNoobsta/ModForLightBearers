4bafaee245b510f72c888eaa1d4c75d86e8d262d
FireNoobsta <FireNoobsta@gmail.com>
2022-11-01 12:55:45 -0700

    Fix batchfile to work with spaces in current directory path

--- LightBearersMOD/InstallMOD.bat
+++ LightBearersMOD/InstallMOD.bat
@@ -16,70 +16,70 @@ echo ^"%CurrDir%\ModFiles\Extract\LightBearers\*^" ^"../../../LightBearers/^">> 
 
 
 ::Extracting the pak file
-%UNREALPAKPATH% %PAKFILEPATH% -Extract %CurrDir%\ModFiles\Extract
+%UNREALPAKPATH% %PAKFILEPATH% -Extract "%CurrDir%\ModFiles\Extract"
 
 ECHO You should have seen a lot of lines saying extractions were made.
 ECHO If not then an error occurred and you should close the program now. Otherwise continue
 PAUSE
 
 ::Replacing files in Extract
-del %CurrDir%\ModFiles\Extract\LightBearers\Config\DefaultInput.ini
-copy %CurrDir%\ModFiles\DefaultInput.ini %CurrDir%\ModFiles\Extract\LightBearers\Config
+del "%CurrDir%\ModFiles\Extract\LightBearers\Config\DefaultInput.ini"
+copy "%CurrDir%\ModFiles\DefaultInput.ini" "%CurrDir%\ModFiles\Extract\LightBearers\Config"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting.uexp
-copy %CurrDir%\ModFiles\Advanced_Lighting.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting.uexp"
+copy "%CurrDir%\ModFiles\Advanced_Lighting.uexp" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting.umap
-copy %CurrDir%\ModFiles\Advanced_Lighting.umap %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting.umap"
+copy "%CurrDir%\ModFiles\Advanced_Lighting.umap" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.uasset
-copy %CurrDir%\ModFiles\Advanced_Lighting_BuiltData.uasset %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.uasset"
+copy "%CurrDir%\ModFiles\Advanced_Lighting_BuiltData.uasset" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.ubulk
-copy %CurrDir%\ModFiles\Advanced_Lighting_BuiltData.ubulk %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.ubulk"
+copy "%CurrDir%\ModFiles\Advanced_Lighting_BuiltData.ubulk" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.uexp
-copy %CurrDir%\ModFiles\Advanced_Lighting_BuiltData.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
-
-
-
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap.uexp
-copy %CurrDir%\ModFiles\StarterMap.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
-
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap.umap
-copy %CurrDir%\ModFiles\StarterMap.umap %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
-
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.uasset
-copy %CurrDir%\ModFiles\StarterMap_BuiltData.uasset %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
-
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.ubulk
-copy %CurrDir%\ModFiles\StarterMap_BuiltData.ubulk %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
-
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.uexp
-copy %CurrDir%\ModFiles\StarterMap_BuiltData.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Advanced_Lighting_BuiltData.uexp"
+copy "%CurrDir%\ModFiles\Advanced_Lighting_BuiltData.uexp" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
 
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default.uexp
-copy %CurrDir%\ModFiles\Minimal_Default.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap.uexp"
+copy "%CurrDir%\ModFiles\StarterMap.uexp" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default.umap
-copy %CurrDir%\ModFiles\Minimal_Default.umap %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap.umap"
+copy "%CurrDir%\ModFiles\StarterMap.umap" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default_BuiltData.uasset
-copy %CurrDir%\ModFiles\Minimal_Default_BuiltData.uasset %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.uasset"
+copy "%CurrDir%\ModFiles\StarterMap_BuiltData.uasset" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default_BuiltData.ubulk
-copy %CurrDir%\ModFiles\Minimal_Default_BuiltData.ubulk %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.ubulk"
+copy "%CurrDir%\ModFiles\StarterMap_BuiltData.ubulk" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
-del %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default_BuiltData.uexp
-copy %CurrDir%\ModFiles\Minimal_Default_BuiltData.uexp %CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\StarterMap_BuiltData.uexp"
+copy "%CurrDir%\ModFiles\StarterMap_BuiltData.uexp" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
+
+
+
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default.uexp"
+copy "%CurrDir%\ModFiles\Minimal_Default.uexp" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
+
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default.umap"
+copy "%CurrDir%\ModFiles\Minimal_Default.umap" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
+
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default_BuiltData.uasset"
+copy "%CurrDir%\ModFiles\Minimal_Default_BuiltData.uasset" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
+
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default_BuiltData.ubulk"
+copy "%CurrDir%\ModFiles\Minimal_Default_BuiltData.ubulk" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
+
+del "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps\Minimal_Default_BuiltData.uexp"
+copy "%CurrDir%\ModFiles\Minimal_Default_BuiltData.uexp" "%CurrDir%\ModFiles\Extract\LightBearers\Content\StarterContent\Maps"
 
 
 ::Deleting pakfile and repaking
 ECHO This next part may take a while, please be patient
 del %PAKFILEPATH%
-%UNREALPAKPATH% %PAKFILEPATH% -Create=%CurrDir%\ModFiles\dataToPack.txt
+%UNREALPAKPATH% %PAKFILEPATH% -Create="%CurrDir%\ModFiles\dataToPack.txt"
 
 ::Deleting Extract, we don't need it anymore
 ::del %CurrDir%\ModFiles\Extract\Engine
